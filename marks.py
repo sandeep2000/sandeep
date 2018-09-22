8f71531fc68e24d410795f53b94b34431c409f34
sandeep2000 <43026069+sandeep2000@users.noreply.github.com>
2018-09-22 12:43:04 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,13 @@
+a=int(input("enter value ofa:"))
+#checking condition
+if(a>=90):
+	print("A")
+elif(a>=80):
+	print("B")
+elif(a>=70):
+	print("C")
+elif(a>=50):
+	print("D")
+else:
+	print("fail")
+
