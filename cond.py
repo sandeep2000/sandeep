bcb8ed6c651d38dbc1e215da2e80fca57b4ff3d3
sandeep2000 <43026069+sandeep2000@users.noreply.github.com>
2018-09-22 12:46:56 +0530

    Add files via upload

---
+++ cond.py
@@ -0,0 +1,17 @@
+x=int(input("enter value of x:"))
+if(x%2):
+ 	print("even")
+#find greatest of two numbers
+a=int(input("enter value of a:"))
+b=int(input("enter value of b:"))
+if(a>b):
+	print("a is greater than b")
+else:
+	print("b is greater than a")
+
+ 
+
+
+
+
+
