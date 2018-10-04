77730e90e416b67d5959c130c39f6c1316e7228a
sandeep2000 <43026069+sandeep2000@users.noreply.github.com>
2018-10-04 13:01:00 +0530

    Add files via upload

--- rps.py
+++ rps.py
@@ -1,14 +1,22 @@
 #dictionary
 import random
 a={1:'r',2:'p',3:'s'}
+while(True):
 
-c=a[random.randint(1,3)]
+	c=a[random.randint(1,3)]	
 
-user_input=input("enter rock paper or scissor")
-print("computer puts",c)
+	user_input=input("enter rock paper or scissor")
+	print("computer puts",c)
 
-#conditions
-if(user_input=='r'and c=='s' or user_input=='p'and c =='r' or user_input=='s'and c =='r'):
-	print("you wins")
-else:
-	print("you lose!!!!")
+
+	#conditions
+	if(user_input=='r'and c=='s' or user_input=='p'and c =='r' or user_input=='s'and c =='r'):
+		print("you wins")
+	else:
+		print("you lose!!!!")
+	t=input("press q to exit")
+	if(t=='q'):
+		exit()
+
+
+
